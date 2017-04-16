5747098394a91062acf43af7729c80fa3dc42753
徐梦华 <15957299@qq.com>
2017-04-16 16:49:33 +0800

    Initial Commit

---
+++ ImageClipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageClipper
+//
+//  Created by Xu Menghua on 2017/4/16.
+//  Copyright © 2017年 Xu Menghua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
